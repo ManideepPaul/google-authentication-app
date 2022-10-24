8e9ba7d01248e2a7b3333442eac7ffd51af6a125
Manideep <17101992m@gmail.com>
2022-10-24 13:14:14 +0530

    comment added to the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Login from './components/Login';
-// import Logout from './components/Logout';
 
 // eslint-disable-next-line
 import { gapi } from 'gapi-script';
+// Added gapi to access people api resources from google.
 
+
+// Google OAuth 2.0 client Id (Without this you can't login with google)
 const clientID = '381842122352-vhun0e9oi2p84t56lsf95q62hbq39odo.apps.googleusercontent.com'
 
 function App() {
   return (
     <div className="App">
+      {/* Adding login component and passing clientID as prop. */}
       <Login clientID={clientID}/>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,65 @@
 import { useState } from 'react';
+
+// A Google oAUth Sign-in / Log-in Component for React
 import { GoogleLogin } from 'react-google-login';
 
+// Importing the Logout component
 import Logout from './Logout';
 
 function Login({ clientID }) {
 
+    // This will be used to update username on page after login
     const [userName, setUserName] = useState('');
-    const [userImage, setUserImage] = useState('');
-    const [logIn, setLogIn] = useState(false);
-    // console.log(logIn , "first")
 
+    // This will be used to update uesr image on page after login
+    const [userImage, setUserImage] = useState('');
+
+    // This will track status of login
+    const [logIn, setLogIn] = useState(false);
+
+    // This function will be called on successfull login
     const responseGoogle = (res) => {
         getDetail(res)
     }
 
+    // This will be called when login is failed
     const failedRes = (res) => {
         console.log(res)
     }
 
+    // After successfull login this function will be called
     const getDetail = (value) => {
+        // Setting the user name from the google response
         setUserName(value.profileObj.name)
+
+        // Setting the image from the google response
         setUserImage(value.profileObj.imageUrl)
+
+        // After successfull login changing the status to true.
         setLogIn(true)
-        // console.log(logIn, "on login")
     }
 
+    // This is called when user click on logout.
     const logOut = () => {
+        // removing the username 
         setUserName('')
+
+        // removing the user image
         setUserImage('')
+
+        // changing the status to false again
         setLogIn(false)
-        // console.log(logIn, "on logout")
     }
 
     return (
         <>
+            {/* If login status is true then show the image */}
             {logIn ? <img src={userImage} alt="userImage" />: ""}
+
+            {/* If login status is true then show user name */}
             {logIn ? <h4>{userName}</h4> : ""}
+
+            {/* If login status is false then only show this button */}
             {!logIn ? <GoogleLogin
                 clientId={clientID}
                 buttonText="Login with Google"
@@ -43,6 +67,8 @@ function Login({ clientID }) {
                 onFailure={failedRes}
                 cookiePolicy={'single_host_origin'}
             /> : ""}
+
+            {/* If login status is true then show this button */}
             {logIn ? <div onClick={logOut}><Logout clientID={clientID} /></div> : ""}
         </>
     )
